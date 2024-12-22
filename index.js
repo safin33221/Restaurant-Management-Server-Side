bec48763223962d2de0bdb7fbf4808aee062807f
safin33221 <saifn33221@gmail.com>
2024-12-22 16:56:16 +0600

    completed to delelted food function form collection

--- index.js
+++ index.js
@@ -62,7 +62,14 @@ async function run() {
         })
 
 
-        
+        //delete food from foodcollection
+        app.delete('/foods/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const filter = { _id: new ObjectId(id) }
+            const result = await foodCollection.deleteOne(filter)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
