74725c9e97cf503a8e1f35f33228c5c9090bc526
safin33221 <saifn33221@gmail.com>
2024-12-23 17:39:02 +0600

    completed to deleted single purchase product

--- index.js
+++ index.js
@@ -124,6 +124,14 @@ async function run() {
             const result = await foodCollection.deleteOne(filter)
             res.send(result)
         })
+        //delete parchases food data
+        app.delete('/food-parchase/:id', async (req, res) => {
+            const id = req.params.id
+            
+            const filter = { _id: new ObjectId(id) }
+            const result = await foodParchaseColleciton.deleteOne(filter)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
